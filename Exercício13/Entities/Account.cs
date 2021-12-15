0fe650a65bf2a7899dca62af46541397d4df2239
Jéssica <jessicabeatrizmonteiro1994@gmail.com>
2021-12-15 00:18:01 -0300

    Projeto Herança

---
+++ Exercício13/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Exercício13.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account() //Construtor vazio permite criar um objeto sem passar os dados
+        {
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Witdraw (double amount)
+        {
+            Balance -= amount; //Subtrai uma quantia do saldo atual "saque"
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount; //Soma uma quantia do saldo atual "depósito"
+        }
+    }
+}
---
+++ Exercício13/Entities/BusinessAccount.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Exercício13.Entities
+{
+    class BusinessAccount : Account //  ": Account" Informa que a classe business Account é a herança da classe Accounnt
+    {
+        public double LoanLimit { get; set; }
+    
+       public BusinessAccount() //Construtor vazio permite criar um objeto sem passar os dados
+        {
+        } 
+    
+        
+       public BusinessAccount ()
+    }
+}
